3009cd31101ba0dab2a1228deb5a91936fcd76ce
Lucas Guilherme <lucassguilherme159@gmail.com>
2024-01-15 20:43:16 -0400

    pulei do Cap 8 para o Cap 9 - por enquanto

---
+++ Cap8/EscolherArquivo/EscolherArquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscolherArquivo
+{
+    public class EscolherArquivo
+    {
+        public static string ObterCaminho()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivos de Texto|*.txt|Todos os Arquivos|*.*"; // Filtre os tipos de arquivos conforme necessário
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            string caminhoDoArquivo = "Nenhum arquivo selecionado.";
+
+            if (result == DialogResult.OK)
+            {
+                caminhoDoArquivo = openFileDialog.FileName;
+
+                return caminhoDoArquivo;
+            }
+            else
+            {
+                return caminhoDoArquivo;
+            }
+
+            
+
+        }
+    }
+}
